0e6ee620a0e60121ce7ad0ad314dc166ebc71d1d
lvxinghai <lvxinghai@mecby.com>
2020-01-01 10:42:28 +0800

    优化

--- src/portal/src/portal.js
+++ src/portal/src/portal.js
@@ -1,50 +1,26 @@
 import * as singleSpa from 'single-spa';
 import { loadApp } from './helper';
 
-const IS_DEV = process.env.NODE_ENV === 'development';
-
 init();
 async function init() {
-  const loadingPromises = [];
-
-  // base应用 VERSION 来源于根目录的package.json 中的version字段
-  // 此处使用的定义在webpack.config.js中
-  loadingPromises.push(
-    loadApp(
-      'base',
-      '',
-      IS_DEV ? `http://localhost:9001/main.js?v${VERSION}`
-        : `/base/main.js?v${VERSION}`
-    )
-  );
-  // batch应用
-  loadingPromises.push(
-    loadApp(
-      'batch',
-      '/batch',
-      IS_DEV ? `http://localhost:9002/main.js?v${VERSION}`
-        : `/batch/main.js?v${VERSION}`
-    )
-  );
-  // inpNurse应用
-  loadingPromises.push(
-    loadApp(
-      'inpNurse',
-      '/inpNurse',
-      IS_DEV ? `http://localhost:9003/main.js?v${VERSION}`
-        : `/inpNurse/main.js?v${VERSION}`
-    )
-  );
-  // sysConfig应用
-  loadingPromises.push(
-    loadApp(
-      'sysConfig',
-      '/sysConfig',
-      IS_DEV ? `http://localhost:9004/main.js?v${VERSION}`
-        : `/sysConfig/main.js?v${VERSION}`
-    )
-  );
+  const loadingPromises = [
+    creatLoadApp('base', 9001), // base应用
+    creatLoadApp('batch', 9002), // batch应用
+    creatLoadApp('inpNurse', 9003), // inpNurse应用
+    creatLoadApp('sysConfig', 9004), // sysConfig应用
+  ];
   await Promise.all(loadingPromises);
 
   singleSpa.start();
+}
+
+// VERSION 来源于根目录的package.json 中的version字段，此处使用的定义在webpack.config.js中
+function creatLoadApp(appName, port) {
+  return loadApp(
+    appName,
+    appName === 'base' ? '' : `/${appName}`,
+    process.env.NODE_ENV === 'development'
+      ? `http://localhost:${port}/main.js?v${VERSION}`
+      : `/${appName}/main.js?v${VERSION}`
+  )
 }
